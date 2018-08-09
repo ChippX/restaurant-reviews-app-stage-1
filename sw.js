719a47cf66f4163c75a0f2e632884ed1db092bae
ChippX <39884518+ChippX@users.noreply.github.com>
2018-08-09 17:30:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -44,11 +44,16 @@ self.addEventListener('activate', function(event) {
 	);
 })
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-		.then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+self.addEventListener("fetch", event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        if (response) {
+          // console.log("[ServiceWorker] Found in cache ", event.request.url);
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+  }
 });
